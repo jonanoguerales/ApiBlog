75d071e5e2e4bbe7a2349a7c540b408f2d41de2f
jonanoguerales <jonafutb@gmail.com>
2024-04-24 22:19:11 +0200

    cambio index

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ app.use(cors(corsOptions));
 import { MongoClient, ServerApiVersion } from "mongodb";
 const uri =
   "mongodb+srv://Ordenador12:Ordenador12@nvo.7shohhw.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -41,15 +40,10 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("Conectado a mongo");
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
